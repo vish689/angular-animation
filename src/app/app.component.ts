de61a4b29260c9bb500d094bf3593016a56e5904
Vishnu Prasad <vish689@gmail.com>
2020-06-14 11:09:32 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', style({opacity: 0}), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75px)', offset: 0}),
+            style({opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0px)', offset: 1}),
+          ]))
+        ]), {optional: true}),
+        query(':leave', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0px)', offset: 0}),
+            style({opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75px)', offset: 1}),
+          ]) )
+        ]), {optional: true}),
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'animation-tut';
+  items = []
+
+  constructor() {
+    this.items = ['Item 1', 'Item 2', 'Item 3'];
+  }
+
+  pushItem() {
+    console.log('adding item')
+    this.items.push('New Item');
+  }
+
+  removeItem() {
+    this.items.pop();
+  }
 }
